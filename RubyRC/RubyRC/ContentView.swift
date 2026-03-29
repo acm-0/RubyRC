d55b75132f4345df6b340804f9e03c42fbba6bac
Adam Malamy <adam.malamy@gmail.com>
2026-03-28 17:25:57 -0700

    just getting started

---
+++ RubyRC/RubyRC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RubyRC
+//
+//  Created by Adam Malamy on 3/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RubyRC/RubyRC/RubyRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubyRCApp.swift
+//  RubyRC
+//
+//  Created by Adam Malamy on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct RubyRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
